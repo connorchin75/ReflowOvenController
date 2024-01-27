3f5281af2cde4da72049a07a8464d27c3816a773
lovedatsnow <84048928+lovedatsnow@users.noreply.github.com>
2024-01-26 19:40:12 -0700

    fixed bugs with function initialization and syntax

--- inc/thermocouple.h
+++ inc/thermocouple.h
@@ -1,4 +1,3 @@
-#include <stdint.h>
 #include <SystemClock.h>
 #include <IOConfig.h>
 #include <GPIO.h>
@@ -14,8 +13,8 @@
 // define pinouts
 #define CS_TEMP 0x01 ////SDIO1/PF0 / pin 7
 //#define MOSI 	0x15 //MOSI1/PD1 / pin 21
-//#define MISO 	0x14 //MISO1/PD0 / pin 20
-//#define SCLK 	0x16 //SCLK1/PD2 / pin 22
+#define MISO 	0x14 //MISO1/PD0 / pin 20
+#define SCLK 	0x16 //SCLK1/PD2 / pin 22
 
 
 void thermocouple_pin_init(void){
@@ -30,109 +29,10 @@ void thermocouple_pin_init(void){
 }
 
 /*********************************/
-/***** HIGH LEVEL FUNCTIONS ******/
-/*********************************/
-
-int getTemp(){
-
-	int temperature = 0;
-	uint16_t rawdata = 0;
-
-	rawdata = readUpperData();
-
-	if ((rawdata & 0x0001) == 0x0001){//if the fault bit (LSB) is 1
-		//error detected
-		xpd_puts("Thermocouple Error detected.\n");
-		getFault();
-		temperature = 0;
-	}
-	else{
-		temperature = rawdata >> 2; //ignore rightmost two bits (reserved and fault bit)
-
-		//temperature is now organized like this:
-		// D13 : Sign
-		// D12 : 2^10
-		// D11 : 2^9
-		// D10 : 2^8
-		// D9  : 2^7
-		// D8  : 2^6
-		// D7  : 2^5
-		// D6  : 2^4
-		// D5  : 2^3
-		// D4  : 2^2
-		// D3  : 2^1
-		// D2  : 2^0
-		// D1  : 2^-1
-		// D0  : 2^-2
-		//note: the 2 LSB are decimal values
-
-		//since the Xinc2 can't handle floats, we will ignore the 2 LSB.
-		temperature >> 2;
-	}
-
-	return temperature;
-}
-
-int getAMBTemp(){
-
-	int ambTemp = 0;
-	uint16_t rawdata = 0;
-
-	rawdata = readLowerData();
-
-	ambTemp = rawdata >> 4; //this will grab the internal / ambient temperature bits. the 4 LSB are flags
-
-	//again, the Xinc2 can't handle floats, so we ignore the bottom 4 bits
-	ambTemp >> 4;
-
-	return ambTemp; //note that ambTemp has a signed bit
-}
-
-uint8_t isFault(){
-
-	uint8_t isFault = 0;
-	uint16_t rawdata = 0;
-
-	rawdata = readUpperData();
-	if ((rawdata & 0x0001) == 0x0001){ //if the fault bit (LSB) is 1
-		isFault = 1;
-	}
-	return isFault;
-}
-
-uint8_t getFault(){
-	xpd_puts("Thermocouple: Entered Fault Function\n");
-	uint8_t faultcode = 0;
-	uint16_t rawdata = 0;
-
-	rawdata = readLowerData();
-	
-	rawdata = rawdata & 0x0001; //grabs the bottom 4 LSBs
-
-	//the bottom 4 bits:
-	// D3 is reserved
-	// D2 is Short to Vcc flag
-	// D1 is Short to Gnd flag
-	// D0 is Open Circuit flag
-	if(((rawdata & 1) == 1) && ((rawdata & 3) != 3))
-		xpd_puts("Thermocouple: Open Circuit detected\n");
-		return 1; //open circuit
-	elsif(((rawdata & 2) == 1) || ((rawdata & 3) == 3))
-		xpd_puts("Thermocouple: Short to Gnd detected\n");
-		return 2; //shorted to Gnd
-	elsif(((rawdata & 4) == 1) || ((rawdata & 3) == 3))
-		xpd_puts("Thermocouple: Short to Vcc detected\n");
-		return 3; //shorted to Vcc
-	else
-		return 0; //Pmod working properly
-}
-
-
-/*********************************/
 /***** LOWER LEVEL FUNCTIONS ******/
 /*********************************/
 
-uint16_t readUpperData(){
+uint16_t readUpperData(void){
 		
 	uint16_t buffer = 0;
 	uint16_t result = 0;
@@ -156,10 +56,10 @@ uint16_t readUpperData(){
 	//turn off CS_temp
 	gpio_write((gpio_read(GPIO_F) | CS_TEMP), GPIO_F); //only changes the CS_TEMP pin to high
 
-	return result
+	return result;
 }
 
-uint16_t readLowerData(){
+uint16_t readLowerData(void){
 		
 	uint16_t buffer = 0;
 	uint16_t result = 0;
@@ -198,5 +98,105 @@ uint16_t readLowerData(){
 	//turn off CS_temp
 	gpio_write((gpio_read(GPIO_F) | CS_TEMP), GPIO_F); //only changes the CS_TEMP pin to high
 
-	return result
+	return result;
+}
+
+
+/*********************************/
+/***** HIGH LEVEL FUNCTIONS ******/
+/*********************************/
+
+int isFault(void){
+
+	int isFault = 0;
+	uint16_t rawdata = 0;
+
+	rawdata = readUpperData();
+	if ((rawdata & 0x0001) == 0x0001){ //if the fault bit (LSB) is 1
+		isFault = 1;
+	}
+	return isFault;
+}
+
+int getFault(void){
+	xpd_puts("Thermocouple: Entered Fault Function\n");
+	uint16_t rawdata = 0;
+
+	rawdata = readLowerData();
+	
+	rawdata = rawdata & 0x0001; //grabs the bottom 4 LSBs
+
+	//the bottom 4 bits:
+	// D3 is reserved
+	// D2 is Short to Vcc flag
+	// D1 is Short to Gnd flag
+	// D0 is Open Circuit flag
+	if(((rawdata & 1) == 1) && ((rawdata & 3) != 3)){
+		xpd_puts("Thermocouple: Open Circuit detected\n");
+		return 1; //open circuit
+	}else if(((rawdata & 2) == 1) || ((rawdata & 3) == 3)){
+		xpd_puts("Thermocouple: Short to Gnd detected\n");
+		return 2; //shorted to Gnd
+	}else if(((rawdata & 4) == 1) || ((rawdata & 3) == 3)){
+		xpd_puts("Thermocouple: Short to Vcc detected\n");
+		return 3; //shorted to Vcc
+	}else{
+		return 0; //Pmod working properly
+	}
+}
+
+
+int getTemp(void){
+
+	int temperature = 0;
+	uint16_t rawdata = 0;
+
+	rawdata = readUpperData();
+
+	if ((rawdata & 0x0001) == 0x0001){//if the fault bit (LSB) is 1
+		//error detected
+		xpd_puts("Thermocouple Error detected.\n");
+		getFault();
+		temperature = 0;
+	}
+	else{
+		temperature = rawdata >> 2; //ignore rightmost two bits (reserved and fault bit)
+
+		//temperature is now organized like this:
+		// D13 : Sign
+		// D12 : 2^10
+		// D11 : 2^9
+		// D10 : 2^8
+		// D9  : 2^7
+		// D8  : 2^6
+		// D7  : 2^5
+		// D6  : 2^4
+		// D5  : 2^3
+		// D4  : 2^2
+		// D3  : 2^1
+		// D2  : 2^0
+		// D1  : 2^-1
+		// D0  : 2^-2
+		//note: the 2 LSB are decimal values
+
+		//since the Xinc2 can't handle floats, we will ignore the 2 LSB.
+		temperature = temperature >> 2;
+	}
+
+	return temperature;
+}
+
+int getAMBTemp(void){
+
+	int ambTemp = 0;
+	uint16_t rawdata = 0;
+
+	rawdata = readLowerData();
+
+	ambTemp = rawdata >> 4; //this will grab the internal / ambient temperature bits. the 4 LSB are flags
+
+	//again, the Xinc2 can't handle floats, so we ignore the bottom 4 bits
+	ambTemp = ambTemp >> 4;
+
+	return ambTemp; //note that ambTemp has a signed bit
 }
