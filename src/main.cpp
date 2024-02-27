32870e3a2e18f0577842ecdc10123f8ef8556b06
lovedatsnow <84048928+lovedatsnow@users.noreply.github.com>
2024-02-26 21:47:37 -0700

    updated semaphores and UI state machine

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,18 @@
 #include "pid.h"
 #include "rotary_encoder.h"
 
+// -- GLOBAL VARIABLES -- //
+int temp = 0; //semaphore 1
+int btn_pressed = 0; //semaphore 2
+int encoder_pos = 0; //semaphore 3
+int humidity = 0; //semaphore 4
+int screen_status = 0; //semaphore 5
+int profile = 0; //semaphore 6
+int chosen_profile = 0; //semaphore 7
+int progress = 0; //semaphore 8
+
+unsigned int* temp_profile[3]; //array of pointers
+
 /// Configures GPIO to be Input/Output, io cell configuration, and clock frequency
 void InitGPIO(){
    //set the system clock to 98.304 MHz 
@@ -58,18 +70,18 @@ void * OLEDThread(void * ) {
 }
 
 void * skeletonOLEDThread(void *){
-   enum scenario {
+   enum scenario_enum {
       START_SCREEN,
       CHANGE_PROFILE,
       DISPLAY_PROGRESS,
       STARTING_PROCESS,
-      EMERGENCY_STOP,
       END_PROCESS
-   }
-   int btn_pressed = 0;
-   int dial_turned = 0;
-   int humidity = 0;
-   int progress = 0;
+   };
+   enum scenario_enum scenario;
+   int last_encoder_pos = 0;
+
+   int MAX_HUMIDITY = 40;
+   
    //start up oled
    OLED_Init_160128RGB();
    //clear oled
@@ -79,44 +91,89 @@ void * skeletonOLEDThread(void *){
          case START_SCREEN:
             OLED_main_page();
             //check if there's a btn press
+            sem_lock(3); //encoder position
+            sem_lock(2); //btn press
             if (btn_pressed == 1){
                //start process
                scenario = STARTING_PROCESS;
             }
-            if (dial_turned == 1){
+            if (encoder_pos != last_encoder_pos){
+               last_encoder_pos = encoder_pos;
                scenario = CHANGE_PROFILE;
             }
+            sem_unlock(3);
+            sem_unlock(2);
             break;
+
+
          case CHANGE_PROFILE:
-            //another state machine
+            sem_lock(2);
+            if (btn_pressed == 1){
+               //start process
+               scenario = START_SCREEN;
+            }
+            sem_unlock(2);
+
+            sem_lock(3); //encoder position
+            if (encoder_pos > last_encoder_pos){ //dial moved clockwise. increase index of profile.
+               last_encoder_pos = encoder_pos;
+               chosen_profile++;
+               OLED_profile_page(chosen_profile);
+            }
+            if (encoder_pos < last_encoder_pos){ //dial moved counter-clockwise. decrease index of profile
+               last_encoder_pos = encoder_pos;
+               chosen_profile--;
+               OLED_profile_page(chosen_profile);
+            }
+            sem_unlock(3);
+            
             break;
-         case STARTING_PROCESS:  
+
+
+         case STARTING_PROCESS:
             OLED_starting_page();
             //this will be a page that will indicate that we are about to start the heating process
+            //add a wait here
             scenario = DISPLAY_PROGRESS;
             break;
+
+
          case DISPLAY_PROGRESS:
             OLED_display_progress();
             if (btn_pressed ==1){
                scenario = END_PROCESS;
             }
-            //read humidity sensor
-            humidity = readhumidity();
+            
+            sem_lock(4);
             //update displayed humidity
+            OLED_update_humidity(humidity);
             //check if it's over the maximum humidity
             if (humidity > MAX_HUMIDITY){
-               //display warning
+               OLED_display_warning(); //might want a new state for this?
             }
-            //read temp
+            sem_unlock(4);
+            
+            sem_lock(1);
             //update displayed temp
+            OLED_update_temp(temp);
+            sem_unlock(1);
+            
             //update displayed progress
+            sem_lock(8);
             if (progress == 100){
                scenario = END_PROCESS;
             }
+            sem_unlock(8);
             break;
-         case EMERGENCY_STOP:
-            break;
+
+
          case END_PROCESS:
+            OLED_end_progress();
+            sem_lock(3);
+            if (btn_pressed ==1){
+               scenario = START_SCREEN;
+            }
+            sem_unlock(3);
             break;
 
       }
@@ -124,16 +181,32 @@ void * skeletonOLEDThread(void *){
 }
 
 void * TempThread(void *) {
-   int current_temp = 0;
+   // int current_temp = 0;
+   // int decimal_temp = 0;
+   // int after_decimal_temp = 0;
    while (true){
-      // xpd_puts("Detected Temp: ");
-      // xpd_echo_int(current_temp, XPD_Flag_SignedDecimal);
-      // xpd_puts(" \n");
-      wait_ms(1000);
-      current_temp = getTemp();
-   }
+      sem_lock(1);
+      temp = getTemp();
+      sem_unlock(1);
 
+   //   xpd_puts("Detected Temp: ");
+   //   xpd_echo_int(current_temp, XPD_Flag_SignedDecimal);
+   //   xpd_puts(" \n");
+
+      	// have not tested the below code that should print the decimal values of temp:
+	// decimal_temp = current_temp >> 2; //2 LSB are decimal points
+      // after_decimal_temp = ((2^-1) *(current_temp & 0x02)) + ((2^-2) * (current_temp & 0x01)); // this shouldd? grab the last two bits to find the value of the decimal places
+
+      // xpd_puts("Decimal value of Temp: ");
+      // xpd_echo_int(decimal_temp, XPD_Flag_SignedDecimal);
+      // xpd_puts(".");
+      // xpd_echo_int(after_decimal_temp, XPD_Flag_UnsignedDecimal);
+      // xpd_puts(" \n");
+
+      wait_ms(1000);
+   }
 }
+
 
 void * RotEncodThread(void *) {
    int encoder_pos = 0;
@@ -188,6 +261,10 @@ int main(void){
    io_set_config(0b1110011, io_PG0);
 
    InitGPIO();
+   
+   temp_profile[0] = generate_RSS_profile(75, 165, 240); //soak, spike, cooling times
+   temp_profile[1] = generate_RSS_profile(30, 30, 180);
+   temp_profile[2] = generate_RSS_profile(60, 75, 200);
 
    thread_setup(OLEDThread, nullptr, 1);
    thread_run(1);
